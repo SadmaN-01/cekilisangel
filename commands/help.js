fa594f2faadb90f83e3d4477fb84d41af3bc9c8c
SadmaN-01 <76557254+SadmaN-01@users.noreply.github.com>
2021-01-09 01:56:32 +0300

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js"),
+client = new Discord.Client();
+const config = require("../config.js");
+const prefix = config.prefix;
+module.exports.run = async (client, message, args) => {
+  if (message.author.bot) return;
+  let prefix = config.prefix;
+  if(!message.content.startsWith(prefix)) return;
+  let help = new Discord.MessageEmbed()
+  .setAuthor("Angel Çekiliş Sistemi")
+  .setColor("YELLOW")
+  .setTitle("Komut Listesi")
+  .addField("🎁 Giveaway 🎁","start [kanal] [süre] [kazananlar] [Ödül]\nend [çekiliş ID]")
+  .addField("Örnek", "g!start #çekiliş 5m 1 Nitro\ng!end Çekiliş ID")
+  .setTimestamp()
+  .setFooter(`Komutu kullanan ${message.author.tag}`, client.user.displayAvatarURL());
+  message.channel.send("**DM adresini kontrol et! 💌**").then(x => x.delete({timeout: 10000}));
+  return message.author.send(help);
+};
+
+exports.config = {
+  name: "çekiliş-sistemi",
+  guildOnly: true,
+  aliases: ["çekiliş-sistemi"],
+};
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "geliştiriciler": ["787225094321078283"], 
+  "prefix": "!"
+};
